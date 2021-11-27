c63f5fdc3fefed15957b0d7f4876e5efd14e0640
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2021-11-27 15:22:41 -0300

    fix: add response model and accessing session attr directly

--- application/modules/user.py
+++ application/modules/user.py
@@ -6,11 +6,11 @@ from ..database.client import DatabaseClient
 from ..models.user import UserModel, UserTable
 
 
-def search_user(id: int = None) -> UserModel:
+def search_user(id: int = None, *, connection: DatabaseClient = None) -> UserModel:
     """Search for a user"""
 
-    with DatabaseClient() as conn:
-        user = conn.session.query(UserTable).get(id)
+    with DatabaseClient(connection=connection) as conn:
+        user = conn.query(UserTable).get(id)
         if not user:
             raise HTTPException(404, "user not found")
 
@@ -22,7 +22,7 @@ def create_user(name: str, cpf: str, birthday: datetime) -> UserModel:
     """Insert new user into database"""
 
     with DatabaseClient() as conn:
-        if conn.session.query(UserTable).filter(UserTable.CPF == cpf).first():
+        if conn.query(UserTable).filter(UserTable.CPF == cpf).first():
             raise HTTPException(400, "user already registered")
 
         user = UserTable(NOME=name, CPF=cpf, DATA_NASCIMENTO=birthday)
--- application/routes/user.py
+++ application/routes/user.py
@@ -6,13 +6,13 @@ from ..routes.payloads.user import CreateUserRequest, CreateUserResponse, Search
 router = APIRouter(prefix="/users", tags=["User"])
 
 
-@router.get("/{id}", summary="Search for a user")
+@router.get("/{id}", summary="Search for a user", response_model=SearchUserResponse)
 def _search_user(id: int) -> SearchUserResponse:
     user = search_user(id)
     return SearchUserResponse(**user.dict())
 
 
-@router.post("/", summary="Create user")
+@router.post("/", summary="Create user", response_model=CreateUserResponse)
 def _create_user(body: CreateUserRequest) -> CreateUserResponse:
     user = create_user(**body.dict())
     return CreateUserResponse(**user.dict())
